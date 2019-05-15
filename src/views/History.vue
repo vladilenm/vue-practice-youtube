2f4d7d276ba666788bc29373324aab43b29b154a
Vladilen <mininvlad@gmail.com>
2019-05-15 14:40:39 +0300

    18

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref="canvas"></canvas>
     </div>
 
     <Loader v-if="loading" />
@@ -34,9 +34,11 @@
 <script>
 import paginationMixin from '@/mixins/pagination.mixin'
 import HistoryTable from '@/components/HistoryTable'
+import {Pie} from 'vue-chartjs'
 
 export default {
   name: 'history',
+  extends: Pie,
   mixins: [paginationMixin],
   data: () => ({
     loading: true,
@@ -46,15 +48,53 @@ export default {
     this.records = await this.$store.dispatch('fetchRecords')
     const categoires = await this.$store.dispatch('fetchCategories')
 
-    this.setupPagination(this.records.map(record => {
-      return {
-        ...record,
-        categoryName: categoires.find(c => c.id === record.categoryId).title,
-        typeClass: record.type === 'income' ? 'green' : 'red',
-        typeText: record.type === 'income' ? 'Доход' : 'Расход',
-      }
-    }))
+    this.setup(categoires)
+    
     this.loading = false
+  },
+  methods: {
+    setup(categoires) {
+      this.setupPagination(this.records.map(record => {
+        return {
+          ...record,
+          categoryName: categoires.find(c => c.id === record.categoryId).title,
+          typeClass: record.type === 'income' ? 'green' : 'red',
+          typeText: record.type === 'income' ? 'Доход' : 'Расход',
+        }
+      }))
+
+      this.renderChart({
+        labels: categoires.map(c => c.title),
+        datasets: [{
+            label: 'Расходы по категориям',
+            data: categoires.map(c => {
+              return this.records.reduce((total, r) => {
+                if (r.categoryId === c.id && r.type === 'outcome') {
+                  total += +r.amount
+                }
+                return total
+              }, 0)
+            }),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+      })
+    }
   },
   components: {
     HistoryTable
